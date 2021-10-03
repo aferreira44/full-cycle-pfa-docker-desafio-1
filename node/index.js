eacf06af50ad2629056f06bc3d6bb6578f12fc92
André Ferreira <contato@andreferreira.me>
2021-10-03 13:05:27 -0300

    Lint and query results

--- node/index.js
+++ node/index.js
@@ -1,28 +1,38 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
 const conn = mysql.createConnection({
-    host: 'pfa-mysql',
-    port: '3306',
-    user: 'root',
-    password: 'root',
-    database: 'pfa',
+  host: "pfa-mysql",
+  port: "3306",
+  user: "root",
+  password: "root",
+  database: "pfa"
 });
 
 conn.connect((err) => {
-    if (err) {
-        console.log("mysql connection error: ", err);
-        return;
-    };
-    console.log('Connected to database');
+  if (err) {
+    console.log("mysql connection error: ", err);
+    return;
+  }
+  console.log("Connected to database");
 });
 
 const app = express();
 
 app.get("/", (_, res) => {
-    res.send("Hello World");
+  conn.query(
+    "SELECT name, description from modules",
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      res.set({ "content-type": "application/json; charset=utf-8" });
+      res.send(results);
+    }
+  );
 });
 
 app.listen(3000, () => {
-    console.log("Server started on port 3000");
+  console.log("Server started on port 3000");
 });
