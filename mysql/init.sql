b3a6f8bbca71dcc575f1d1f8665dce2ee8bd439c
André Ferreira <contato@andreferreira.me>
2021-10-03 13:06:07 -0300

    Create table and insert modules data

--- mysql/init.sql
+++ mysql/init.sql
@@ -2,4 +2,28 @@ CREATE DATABASE pfa;
 
 use pfa;
 
--- INSERTS;
+SET NAMES utf8mb4;
+
+CREATE TABLE modules (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(140) NOT NULL
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+INSERT INTO modules (name, description)
+VALUES ("Docker", "Módulo 1");
+
+INSERT INTO modules (name, description)
+VALUES ("Comunicação", "Módulo 2");
+
+INSERT INTO modules (name, description)
+VALUES ("RabbitMQ", "Módulo 3");
+
+INSERT INTO modules (name, description)
+VALUES ("Apache Kafka", "Módulo 4");
+
+INSERT INTO modules (name, description)
+VALUES ("Autenticação e Keycloak", "Módulo 5");
+
+INSERT INTO modules (name, description)
+VALUES ("Arquitetura Hexagonal", "Módulo 6");
